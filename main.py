7120f6395ef875d64152a81334a2801f4b2e96d1
Jan Poonthong <janpoonthong628@gmail.com>
2024-07-27 08:09:11 +0700

    refactor: Correct spelling, improve Docstrings, and remove unused variables

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-"DOCS"
+"""DOCS"""
 import os
 import requests
 from dotenv import load_dotenv
@@ -6,11 +6,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-class RequestHadler:
-    "Handle HTTP requests"
+class RequestHandler:
+    """Handle HTTP requests"""
+
     @staticmethod
-    def safe_request(url: str, headers={}, allow_redirects=True):
-        "Safely request a url"
+    def safe_request(url: str, headers: dict, allow_redirects=True):
+        """Safely request a url"""
 
         response = None
         try:
@@ -28,45 +29,45 @@ class RequestHadler:
 
 
 class GitHubSearcher:
-    "Handle GitHub code searching"
+    """Handle GitHub code searching"""
 
     def __init__(self, token):
         self.token = token
 
     @staticmethod
     def write_to_file(response):
-        "DOCS"
+        """DOCS"""
         file_name = "response.json"
         try:
-            with open(file_name, 'w') as file:
+            with open(file_name, "w") as file:
                 file.write(response.text)
         except Exception as error:
             print(f"ERROR: writing on {file_name}")
             raise Exception(error)
 
     def github_code_search(self):
-        "Search goo.gl on GitHub"
+        """Search goo.gl on GitHub"""
         url = "https://api.github.com/search/code?q=https://goo.gl"
 
         headers = {"Authorization": f"Token {self.token}"}
 
-        response = RequestHadler.safe_request(url, headers=headers)
+        response = RequestHandler.safe_request(url, headers=headers)
         self.write_to_file(response)
         return response.json()
 
     @staticmethod
     def get_destination_url(url) -> str:
-        "Get the 'location' from response header to get final destination url"
+        """Get the 'location' from response header to get final destination url"""
 
-        response = RequestHadler.safe_request(url, allow_redirects=False)
+        response = RequestHandler.safe_request(url, headers={}, allow_redirects=False)
 
         location = response.headers["location"]
         return location
 
 
 def main():
-    "Starting of the program"
-    token = os.getenv('GITHUB_TOKEN')
+    """Starting of the program"""
+    token = os.getenv("GITHUB_TOKEN")
     if not token:
         print("GITHUB_TOKEN is not set in the environment variables.")
         return
